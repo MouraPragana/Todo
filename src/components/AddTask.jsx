4c26055a8fc7b13f620be544dfb168f52782f1ad
Mateus Pragana <mateuspragana@gmail.com>
2022-01-24 11:46:54 -0300

    Criando elemento addTask mais sua estilização

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button.jsx";
+import "./AddTask.css";
+
+const AddTask = () => {
+  return (
+    <div className="add-task-container">
+      <input className="add-task-input" type="text" />
+      <div className="add-task-button-container">
+        <Button>Adicionar</Button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
