01fd39fce6edd625e12e8ab6d0d2abd6fce44fe8
Mateus Pragana <mateuspragana@gmail.com>
2022-01-25 20:16:32 -0300

    Criando botão de remoção de tarefas e suas funções

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,34 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { CgClose, CgInfo } from "react-icons/cg";
+
 import "./Task.css";
 
-const Task = ({ task }) => {
-  return <div className="task-container">{task.title}</div>;
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+  return (
+    <div
+      className="task-container"
+      style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+    >
+      <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+        {task.title}
+      </div>
+      <div className="buttons-container">
+        <button
+          onClick={() => handleTaskDeletion(task.id)}
+          className="remove-task-button"
+        >
+          <CgClose />
+        </button>
+        <button
+          onClick={() => handleTaskDeletion(task.id)}
+          className="see-task-details-button"
+        >
+          <CgInfo />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
