86e56e3c155ea3e5fc680272652cb476c9357efb
Mateus Pragana <mateuspragana@gmail.com>
2022-01-26 18:30:58 -0300

    Criando tratativas de retornar erro caso o campo esteja em branco. Há estilização visual caso haja erro (placeholder em vermelho e bordar vermelha).

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -5,19 +5,27 @@ import "./AddTask.css";
 
 const AddTask = ({ handleTaskAddition }) => {
   const [inputData, setInputData] = useState("");
+  const [errorInput, setErrorInput] = useState(false);
 
   const handleInputChange = (e) => {
     setInputData(e.target.value);
   };
 
   const handleAddTaskClick = () => {
-    handleTaskAddition(inputData);
+    if (inputData.trim().length > 0){
+      handleTaskAddition(inputData.trim());
+      setErrorInput(false);
+    } else {
+      setErrorInput(true);
+    }
     setInputData("");
   };
 
   return (
     <div className="add-task-container">
       <input
+        style={errorInput ? { border: "1px solid red" } : { border: "none" }}
+        placeholder={errorInput ? "Atividade inválida" : null}
         onChange={handleInputChange}
         value={inputData}
         className="add-task-input"
