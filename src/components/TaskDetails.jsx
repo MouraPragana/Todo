e6506bdb3dc05ee6637a547f832544567aad8a5d
Mateus Pragana <mateuspragana@gmail.com>
2022-01-26 12:54:02 -0300

    Na pagina taskdetail criando funcionalidade para voltar para página anterior

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,14 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import Button from "./Button.jsx";
 import "./TaskDetails.css";
 
 const TaskDetails = () => {
+  const navigate = useNavigate();
   const params = useParams();
+
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <div className="back-button-container">
-        <Button>Voltar</Button>
+        <Button onClick={handleBackButtonClick}>Voltar</Button>
       </div>
       <div className="task-details-container">
         <h2>{params.taskTitle}</h2>
